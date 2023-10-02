32355f03f3adca6f8511165b23c4fdcfafce9ad9
Sourav Ghorai <ghoraisourav451@gmail.com>
2023-10-03 02:04:25 +0530

    Copy to clipboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -9,6 +9,10 @@ function App() {
 
   const [password, setPassword] = useState("")
 
+  //Reference hook using useRef
+  const passwordRef = useRef(null)
+
+  //Defining the function for password generating use useCallback hook for having some dependencies
   const passwordGenerator = useCallback(() =>{
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -27,33 +31,46 @@ function App() {
 
   },[length, number, character, setPassword])
 
+  //defining the function for coping the generated password into clip board
+  const copyToClipBoard = useCallback(()=>{
+    passwordRef.current?.select()                   //Seleting the password on copying
+    passwordRef.current?.setSelectionRange(0,50)    //setting range how much portion to be selected
+    window.navigator.clipboard.writeText(password) //Coping the password on clip board
+  }, [password])
+
+  //Calling the passwordGenerator function using useEffect hook
   useEffect(() => {
     passwordGenerator()
-  },[length,number, character, passwordGenerator])
+  },[length, number, character, passwordGenerator])
 
   return (
     <>
-      {/* Password Generator Main div*/}
-      <div className="w-full max-w-md mx-auto p-4 my-8 shadow-md rounded-lg bg-gray-600 text-orange-500">
+      {/* Password Generator Main div */}
+      <div className="w-full max-w-md mx-auto p-4 my-8 shadow-md rounded-lg bg-[#2E4F4F] text-[#CBE4DE]">
 
         {/* Heading  */}
-        <h2 className="text-2xl text-white my-4">Password Generator</h2>
+        <h2 className="text-2xl my-4">Password Generator</h2>
 
         {/* Input box */}
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input 
             type="text" 
             value={password}
-            className="outline-none w-full py-1 px-3 bg-slate-200"
+            className="outline-none w-full py-1 px-3 bg-white shadow-lg text-[#1c6965b4]"
             placeholder='Password'
             readOnly
+            ref={passwordRef}
           />
           {/* Copy Button */}
-          <button className="bg-blue-500 outline-none px-3 py-0.5 shrink-0 rounded-none text-white">Copy</button>
+          <button 
+            onClick={copyToClipBoard}
+            className="bg-[#0e8488] hover:bg-[#3ba1a5] outline-none 
+                        px-3 py-0.5 shrink-0 rounded-none text-white"
+          >Copy</button>
         </div>
 
         {/* Functionalities of the password generator  */}
-        <div className="flex flex-wrap text-sm gap-x-2 justify-center">
+        <div className="flex flex-wrap text-sm gap-x-3 justify-center">
           {/* Setting length */}
           <div className="flex items-center gap-x-1">
             <input 
@@ -80,6 +97,7 @@ function App() {
             <label>Numbers</label>      
           </div>
 
+          {/* Adding Character into password  */}
           <div className="flex items-center gap-x-1">
             <input 
               type="checkbox" 
